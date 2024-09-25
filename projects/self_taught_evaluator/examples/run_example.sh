947e80066023a429b1d5d9a37b6715f5a7d8f9b8
Ilia Kulikov <kulikov@meta.com>
2024-09-25 18:21:23 +0000

    use public model dirs

--- projects/self_taught_evaluator/examples/run_example.sh
+++ projects/self_taught_evaluator/examples/run_example.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-HELPSTEER2_VALIDATION=helpsteer2_validation.jsonl
-MODEL_DIR=/opt/hpcaas/.mounts/fs-0e3f1457c6d924fc0/kulikov/openeft_fs2_ckpts/wildchat_dpo_it1_public_recipe/checkpoints_hf/step_300_hf
-RESULTS_FILEPATH="example_outputs.jsonl"
+EXAMPLE_INPUTS=example_inputs.jsonl
+MODEL_DIR=facebook/Self-taught-evaluator-llama3.1-70B  # HF repo
+EXAMPLE_OUTPUTS="example_outputs.jsonl"
 
-python run_model.py --inputs_jsonl_path=$HELPSTEER2_VALIDATION --model_dir=$MODEL_DIR --results_save_path=${RESULTS_FILEPATH}
+python run_model.py --inputs_jsonl_path=$EXAMPLE_INPUTS --model_dir=$MODEL_DIR --results_save_path=${EXAMPLE_OUTPUTS}
