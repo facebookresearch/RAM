8158fe01161c3c47829fa7e90d922da0bcb0a35a
Ilia Kulikov <kulikov@meta.com>
2024-09-25 18:23:04 +0000

    small fix

--- projects/self_taught_evaluator/examples/run_example.sh
+++ projects/self_taught_evaluator/examples/run_example.sh
@@ -2,6 +2,6 @@
 
 EXAMPLE_INPUTS=example_inputs.jsonl
 MODEL_DIR=facebook/Self-taught-evaluator-llama3.1-70B  # HF repo
-EXAMPLE_OUTPUTS="example_outputs.jsonl"
+EXAMPLE_OUTPUTS=example_outputs.jsonl
 
 python run_model.py --inputs_jsonl_path=$EXAMPLE_INPUTS --model_dir=$MODEL_DIR --results_save_path=${EXAMPLE_OUTPUTS}
