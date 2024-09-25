c442201ded8dfef79427305a0529d2ced1e980b2
Ilia Kulikov <kulikov@meta.com>
2024-09-25 18:46:59 +0000

    typo

--- projects/self_taught_evaluator/src/utils.py
+++ projects/self_taught_evaluator/src/utils.py
@@ -9,7 +9,7 @@ from rewardbench.constants import EXAMPLE_COUNTS, SUBSET_MAPPING
 from rewardbench.utils import calculate_scores_per_section
 from transformers import LlamaTokenizer
 
-SELF_TAUGHT_WITH_SYSTEM_PROMT = [
+SELF_TAUGHT_WITH_SYSTEM_PROMPT = [
     {
         "role": "system",
         "content": 'Please act as an impartial judge and evaluate the quality of the responses provided by two AI assistants to the user question displayed below. You should choose the assistant that follows the user\'s instructions and answers the user\'s question better. Your evaluation should consider factors such as the helpfulness, relevance, accuracy, depth, creativity, and level of detail of their responses. Begin your evaluation by comparing the two responses and provide a short explanation. Avoid any position biases and ensure that the order in which the responses were presented does not influence your decision. Do not allow the length of the responses to influence your evaluation. Do not favor certain names of the assistants. Be as objective as possible. After providing your explanation, output your final verdict by strictly following this format: \\"[[A]]\\" if assistant A is better, \\"[[B]]\\" if assistant B is better.',
@@ -53,7 +53,7 @@ def save_to_jsonl(data: List[Dict], filename: str, write_mode="w"):
 def prepare_vllm_input(
     input: str, response_a: str, response_b: str, tokenizer: LlamaTokenizer
 ):
-    conversation = SELF_TAUGHT_WITH_SYSTEM_PROMT
+    conversation = SELF_TAUGHT_WITH_SYSTEM_PROMPT
     conversation[-1]["content"] = conversation[-1]["content"].format(
         **{
             "input": input,
