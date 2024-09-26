9da2c6b14872c9b1603aa8c98461027140c3b42f
Ilia Kulikov <kulikov@cs.nyu.edu>
2024-09-26 14:54:33 -0700

    Update projects/self_taught_evaluator/run_inference_wvllm.sh
    Co-authored-by: Golovneva <103262907+Golovneva@users.noreply.github.com>

--- projects/self_taught_evaluator/run_inference_wvllm.sh
+++ projects/self_taught_evaluator/run_inference_wvllm.sh
@@ -6,7 +6,7 @@
 # LICENSE file in the root directory of this source tree.
 
 EXAMPLE_INPUTS=example_inputs.jsonl
-MODEL_DIR=/fsx-ram/shared/Self-taught-evaluator-llama3.1-70B/dpo_model  # CHANGEME
+MODEL_DIR=./Self-taught-evaluator-llama3.1-70B/dpo_model  # CHANGEME
 EXAMPLE_OUTPUTS=./example_outputs.jsonl
 
 python src/run_model.py --inputs_jsonl_path=$EXAMPLE_INPUTS --model_dir=$MODEL_DIR --results_save_path=${EXAMPLE_OUTPUTS}
