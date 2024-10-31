f2e3aa821a96effe31a049e89fa897977320b039
tkrulewich <77564096+tkrulewich@users.noreply.github.com>
2024-10-31 11:25:17 -0500

    Update utils.py
    Added deepcopy to SELF_TAUGHT_WITH_SYSTEM_PROMPT in prepare_vllm_input. Fixes bug due to global mutable list being modified. After first call datapoint, all subsequent output was identical.

--- projects/self_taught_evaluator/src/utils.py
+++ projects/self_taught_evaluator/src/utils.py
@@ -5,6 +5,7 @@ This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 """
 
+from copy import deepcopy
 import json
 from typing import Any, Dict, Iterable, List
 
@@ -58,7 +59,7 @@ def save_to_jsonl(data: List[Dict], filename: str, write_mode="w"):
 def prepare_vllm_input(
     input: str, response_a: str, response_b: str, tokenizer: LlamaTokenizer
 ):
-    conversation = SELF_TAUGHT_WITH_SYSTEM_PROMPT
+    conversation = deepcopy(SELF_TAUGHT_WITH_SYSTEM_PROMPT)
     conversation[-1]["content"] = conversation[-1]["content"].format(
         **{
             "input": input,
