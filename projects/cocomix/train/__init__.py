5c1059e13768c7c18312a4c8bfd194f63da1532c
jihoontack <reallifehoon@kaist.ac.kr>
2025-02-12 11:26:57 +0900

    run code style check

--- projects/cocomix/train/__init__.py
+++ projects/cocomix/train/__init__.py
@@ -5,6 +5,7 @@ This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 """
 
+
 def setup(mode, cfg):
 
     base_model = cfg.base_model
